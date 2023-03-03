e84e5ab2fe0ea7235ca5bcfeba079321f0b2add0
Denis Denisov <denisofflive@mail.ru>
2023-03-03 20:15:25 +0300

    add update project

--- conftest.py
+++ conftest.py
@@ -2,11 +2,10 @@ import pytest
 from selene.support.shared import browser
 
 @pytest.fixture()
-def window_size():
-    browser.config.window_width = 1024
-    browser.config.window_height = 768
-
-@pytest.fixture()
-def base_url():
+def browser_setup():
+    browser.config.window_width = 1440
+    browser.config.window_height = 1080
     browser.open('https://demoqa.com/automation-practice-form')
-    browser.config.driver.maximize_window()
+
+    # browser.config.driver.maximize_window()
+
--- test_form_demoqa.py
+++ test_form_demoqa.py
@@ -5,7 +5,7 @@ from selene import be, have
 import random
 
 
-def test_form_demoqa(base_url):
+def test_form_demoqa(browser_setup):
     browser.element('#firstName').should(be.blank).type('Denis')
     browser.element('#lastName').should(be.blank).type('Denisov')
     browser.element('#userEmail').should(be.blank).type('denis@denisov.com')
@@ -17,7 +17,7 @@ def test_form_demoqa(base_url):
     browser.element('.react-datepicker__day--015').should(have.text('15')).click()
     browser.element('#subjectsInput').type('Maths').press_enter()
     browser.element(f"div[class*='custom-control'] label[for='hobbies-checkbox-{random.randint(1, 3)}']").click()
-    browser.element('#uploadPicture').send_keys("E:/qa_guru_python_4_5/001.jpg")
+    browser.element('#uploadPicture').send_keys("C:/Users/Denisoff/Desktop/1.jpg")
     browser.element('#currentAddress').should(be.blank).type('Moscow')
     browser.element('#state').click()
     browser.element('#react-select-3-option-1').should(have.text('Uttar Pradesh')).click()
